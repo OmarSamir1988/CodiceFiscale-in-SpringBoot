77e4a70145ca9c1c4dabf5e6584631ed8090af98
Omar Samir <129541680+OmarSamir1988@users.noreply.github.com>
2024-05-05 00:43:47 +0200

    Add files via upload

---
+++ CodiceFiscaleLast/src/main/java/it/ifm/CodiceFiscale/CodiceFiscaleLast/CodiceFiscaleLastApplication.java
@@ -0,0 +1,14 @@
+package it.ifm.CodiceFiscale.CodiceFiscaleLast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodiceFiscaleLastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodiceFiscaleLastApplication.class, args);
+		
+	}
+
+}
---
+++ CodiceFiscaleLast/src/test/java/it/ifm/CodiceFiscale/CodiceFiscaleLast/CodiceFiscaleLastApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ifm.CodiceFiscale.CodiceFiscaleLast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodiceFiscaleLastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
